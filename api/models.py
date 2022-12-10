c38cef88599387372db07da576d2ffaff3060543
EmirlanDogdurbaev <jigit38@gmail.com>
2022-12-10 22:18:12 +0600

    test api

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(null=True)
     text = models.TextField(max_length=500)
 
     def __str__(self) :
